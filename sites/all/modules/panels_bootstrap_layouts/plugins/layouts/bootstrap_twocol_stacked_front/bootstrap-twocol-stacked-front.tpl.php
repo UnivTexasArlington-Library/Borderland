adb267248423a8e9966b0bb97e895b7082ca773e
Krystal Schenk <kschenk@uta.edu>
2016-12-09 12:40:06 -0600

    bootstrap front layout

---
+++ sites/all/modules/panels_bootstrap_layouts/plugins/layouts/bootstrap_twocol_stacked_front/bootstrap-twocol-stacked-front.tpl.php
@@ -0,0 +1,16 @@
+<div class="<?php print $classes ?> container-fluid" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="row">
+    <?php print $content['top']; ?>
+  </div>
+  <div class="row">
+    <div class="col-lg-2 col-md-2 hidden-sm hidden-xs">
+    <?php print $content['left']; ?>
+    </div>
+    <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12">
+    <?php print $content['right']; ?>
+    </div>
+  </div>
+  <div class="row">
+    <?php print $content['bottom']; ?>
+  </div>
+</div>
