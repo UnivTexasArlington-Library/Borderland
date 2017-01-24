80bf9ffda1d85edd6faa56342c160336872c6431
Krystal Schenk <kschenk@uta.edu>
2017-01-24 12:24:09 -0600

    front page template 3-9

--- sites/all/themes/borderland_sass/templates/bootstrap-twocol-stacked-front.tpl.php
+++ sites/all/themes/borderland_sass/templates/bootstrap-twocol-stacked-front.tpl.php
@@ -3,9 +3,9 @@
     <?php print $content['top']; ?>
   </div>
   <div class="row">
-    <div class="col-lg-2 col-md-2 col-sm-2 hidden-xs">
+    <div class="col-lg-2 col-md-2 col-sm-3 hidden-xs">
     <?php print $content['left']; ?></div>
-    <div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
+    <div class="col-lg-10 col-md-10 col-sm-9 col-xs-12">
     <?php print $content['right']; ?>
   </div>
   </div>
