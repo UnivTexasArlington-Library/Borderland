63d2fee5e0b64b8943d81f70a2f2fd05005aa691
Krystal Schenk <kschenk@uta.edu>
2017-01-24 12:04:26 -0600

    template front page change

--- sites/all/modules/panels_bootstrap_layouts/plugins/layouts/bootstrap_twocol_stacked_front/bootstrap-twocol-stacked-front.tpl.php
+++ sites/all/modules/panels_bootstrap_layouts/plugins/layouts/bootstrap_twocol_stacked_front/bootstrap-twocol-stacked-front.tpl.php
@@ -3,10 +3,10 @@
     <?php print $content['top']; ?>
   </div>
   <div class="row">
-    <div class="col-lg-2 col-md-2 hidden-sm hidden-xs">
+    <div class="col-lg-2 col-md-2 col-sm-2 hidden-xs">
     <?php print $content['left']; ?>
     </div>
-    <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12">
+    <div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
     <?php print $content['right']; ?>
     </div>
   </div>
