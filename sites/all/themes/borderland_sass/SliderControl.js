66e7ae0ad103744163d3db89cc2f06542b7b826e
Krystal Schenk <kschenk@uta.edu>
2018-09-24 09:56:59 -0500

    Revert "update marker labels in popups on main map"
    This reverts commit f88245ddbf9b2795379d143a034872620a77a9f1.

--- sites/all/themes/borderland_sass/SliderControl.js
+++ sites/all/themes/borderland_sass/SliderControl.js
@@ -174,6 +174,7 @@ L.Control.SliderControl = L.Control.extend({
     							    _options.markers[i].bindPopup('<a href="/borderland/event/'+_options.markers[i].feature.properties.Nid+'">'+_options.markers[i].feature.properties.name+'</a>'+'<br><br>'+_options.markers[i].feature.properties.Date+'<br>'+
                                		_options.markers[i].feature.properties.description+'<br><br>'+'<strong>Time Period: </strong>'+'<a href="/borderland/period/'+timeString+'">'+_options.markers[i].feature.properties.timePeriod+'</a>'+'<br>'+
                                		'<strong>Ethnic Group: </strong>'+_options.markers[i].feature.properties.EthnicGroup+'<br>'+'<strong>Tribe: </strong>'+_options.markers[i].feature.properties.Tribe+'<br>'+'<strong>Gender: </strong>'+_options.markers[i].feature.properties.Gender+
+                               		'<br>'+'<strong>Activity: </strong>'+_options.markers[i].feature.properties.Activity);
                                		_options.markers[i].bindTooltip(_options.markers[i].feature.properties.name,{offset: [0,-25]});
 
     							if (_options.markers[i].feature.properties.timePeriod == 'Mexican Era 1821-1835')
@@ -233,7 +234,7 @@ L.Control.SliderControl = L.Control.extend({
     							// intially add all the markers and add all the points to the evet list
     							_options.markers[i].bindPopup('<a href="/borderland/event/'+_options.markers[i].feature.properties.Nid+'">'+_options.markers[i].feature.properties.name+'</a>'+'<br><br>'+_options.markers[i].feature.properties.Date+'<br>'+
                                	_options.markers[i].feature.properties.description+'<br><br>'+'<strong>Time Period: </strong>'+'<a href="/borderland/period/'+timeString+'">'+_options.markers[i].feature.properties.timePeriod+'</a>'+'<br>'+'<strong>Ethnic Group: </strong>'+_options.markers[i].feature.properties.EthnicGroup+'<br>'+
-                               	'<strong>Tribe: </strong>'+_options.markers[i].feature.properties.Tribe+'<br>'+'<strong>Gender: </strong>'+_options.markers[i].feature.properties.Gender);
+                               	'<strong>Tribe: </strong>'+_options.markers[i].feature.properties.Tribe+'<br>'+'<strong>Gender: </strong>'+_options.markers[i].feature.properties.Gender+'<br>'+'<strong>Activity: </strong>'+_options.markers[i].feature.properties.Activity);
                                	_options.markers[i].bindTooltip(_options.markers[i].feature.properties.name,{offset: [0,-25]});
 
     							if (_options.markers[i].feature.properties.timePeriod == 'Mexican Era 1821-1835')
