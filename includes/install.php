450ffd7442ebec6c5dc5b46a3c082057433549af
aleverenz <aleverenz@users.noreply.github.com>
2024-10-03 14:20:34 -0500

    Update to Drupal core 7.101

---
+++ includes/install.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * Initiates a browser-based installation of Drupal.
+ */
+
+/**
+ * Defines the root directory of the Drupal installation.
+ */
+define('DRUPAL_ROOT', getcwd());
+
+/**
+ * Global flag to indicate the site is in installation mode.
+ */
+define('MAINTENANCE_MODE', 'install');
+
+// Exit early if running an incompatible PHP version to avoid fatal errors.
+if (version_compare(PHP_VERSION, '5.6.0') < 0) {
+  print 'Your PHP installation is too old. Drupal requires at least PHP 5.6. See the <a href="https://www.drupal.org/docs/7/system-requirements">system requirements</a> page for more information.';
+  exit;
+}
+
+// Start the installer.
+require_once DRUPAL_ROOT . '/includes/install.core.inc';
+install_drupal();
